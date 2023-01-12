953bef6d71f87a802c026fedac6b75abc9bb6274
GabiLivia <100616969+GabiLivia@users.noreply.github.com>
2023-01-12 17:09:02 +0200

    Updates

---
+++ precache-manifest.8dc35b8a746bd22e3cb01fa550037d69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d4ae9c7831764e63547c5218589eb4",
+    "url": "/Crave-React-Project/index.html"
+  },
+  {
+    "revision": "ff26738c7ce34fe1158e",
+    "url": "/Crave-React-Project/static/css/main.f0cf3fcd.chunk.css"
+  },
+  {
+    "revision": "4d57e28cdc4473d7038c",
+    "url": "/Crave-React-Project/static/js/2.c3554584.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Crave-React-Project/static/js/2.c3554584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff26738c7ce34fe1158e",
+    "url": "/Crave-React-Project/static/js/main.ad8926fc.chunk.js"
+  },
+  {
+    "revision": "d93d0403e8d4f881914a",
+    "url": "/Crave-React-Project/static/js/runtime-main.b49fb8db.js"
+  },
+  {
+    "revision": "19ea9f7b2f9c35a6300601265dacf5ad",
+    "url": "/Crave-React-Project/static/media/cheese.19ea9f7b.png"
+  },
+  {
+    "revision": "5f37d5a141e8adb9d138ef1af935e5c8",
+    "url": "/Crave-React-Project/static/media/circles.5f37d5a1.png"
+  },
+  {
+    "revision": "4cdfca781a0f143e5d228957f2cf9efd",
+    "url": "/Crave-React-Project/static/media/compressed-image.4cdfca78.png"
+  },
+  {
+    "revision": "f6a3284a0d95c129ff551ed099c52765",
+    "url": "/Crave-React-Project/static/media/paprika.f6a3284a.png"
+  },
+  {
+    "revision": "71efbddeb61e57eef946ee7a17e240f4",
+    "url": "/Crave-React-Project/static/media/sourcream.71efbdde.png"
+  }
+]);
